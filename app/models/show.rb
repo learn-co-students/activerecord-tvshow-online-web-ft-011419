3d6d972e0a89a87d1118a5b4e72ca2d04c36b2ae
Sim Greenbaum <gbs4ever2@gmail.com>
2019-02-20 04:29:54 +0000

    Done.

--- app/models/show.rb
+++ app/models/show.rb
@@ -17,4 +17,19 @@ def self.least_popular_show
 end
 
 
+  def self.ratings_sum
+    self.sum(:rating)
+  end
+  def self.popular_shows
+    self.where("rating >= ?",5)
+  end
+
+def self.shows_by_alphabetical_order
+  self.order(:name)
+end
+
+
+
+
+
 end
